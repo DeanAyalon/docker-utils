256c22e1e67b3cfd656a05f9569afc1372789abe
Dean Ayalon <deanayalon@me.com>
2024-07-01 18:44:02 +0300

    Reimagined dozzle script

--- dozzle.sh
+++ dozzle.sh
@@ -1,20 +1,65 @@
 #!/bin/bash
 
-# Check input
-if [ -z "$1" ]; then
-    echo No log file selected
-    exit 1
-elif [ ! -f "$1" ]; then
-    echo "$1" is not a file
-    exit 1
-fi
+# Help
+help() {
+    echo "Use: dozzle.sh [options]"
+    echo Options:
+    echo "  -h      Display this help dialog"
+    echo "  -o      Start Dozzle and open it in browser"
+    echo "  -O      Open Dozzle in browser"
+    echo "  -s      Stop dozzle"
+    echo "  -w      Watch log file"
+}
 
-# Container name
-if [ ! -z "$2" ]; then
-    container=$2
-else
-    container=$(basename "$1")
-fi
+open_dozzle() {
+    # Check Dozzle
+    if [ -z "$(docker ps -q --filter name=dozzle)" ]; then
+        echo Dozzle is down
+        exit 1
+    fi
 
-# Start tracking container
-docker run -dth "$container" -v "$1:/log/log.log:ro" --name "$container" alpine sh -c "cat /log/log.log; tail -f /log/log.log"
+    # TODO: open by OS
+    if [ $(uname) = "Darwin" ]; then
+        open $url
+    else
+        echo OS not yet supported, please manually open this url:
+        echo $url
+    fi
+}
+
+# Constants
+url=http://localhost:9999
+
+# SCRIPT EXECUTION
+
+# Options
+while getopts "hoOsw:" opt; do
+    case ${opt} in
+        h ) help; exit 2 ;;
+
+        o ) open=true ;;
+        O ) open_dozzle; exit 0 ;;
+
+        # Stop
+        s ) cd $(dirname "$0")
+            docker compose down dozzle
+            exit 0 
+            ;;
+
+        # Watch - alpine container tailing specified file
+        w ) container=$(basename "$OPTARG")
+            echo Starting container $container to watch $OPTARG
+            docker run -dth "$container" --name $container \
+                -v "$OPTARG:/log:ro" alpine \
+                sh -c "cat /log; tail -f /log" 
+            ;;
+
+        ? ) help; exit 1 ;; # Wrong input
+    esac
+done
+shift "$((OPTIND-1))"
+
+cd "$(dirname "$0")"
+docker compose up -d dozzle
+
+[ -z $open ] || open_dozzle
